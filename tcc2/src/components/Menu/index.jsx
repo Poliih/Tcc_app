fd517fb335353930d199619025e0bfe4933ccb8e
Poliana Rodrigues <104945360+Poliih@users.noreply.github.com>
2024-05-06 23:59:11 -0300

    up

--- tcc2/src/components/Menu/index.jsx
+++ tcc2/src/components/Menu/index.jsx
@@ -1,59 +1,47 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faAngleRight, faCog, faChartPie, faDatabase, faBrain } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faCog, faChartPie, faDatabase, faBrain } from '@fortawesome/free-solid-svg-icons';
 
 const MenuComponent = ({ menuAberto, toggleMenu }) => {
-  const handleCloseMenu = () => {
-    if (menuAberto) {
-      toggleMenu();
-    }
-  };
-
-  const expandMenu = () => {
-    toggleMenu();
-  };
-
-  const collapseMenu = () => {
+  const handleToggleMenu = () => {
     toggleMenu();
   };
 
   return (
     <div className={`menu ${menuAberto ? '' : 'fechado'}`}>
-      {menuAberto && (
-        <div className="botao-recolher" onClick={collapseMenu}>
+      <div className="icones-menu">
+        <div className={`botao-recolher ${menuAberto ? '' : 'rotate'}`} onClick={handleToggleMenu}>
           <FontAwesomeIcon icon={faAngleLeft} />
         </div>
-      )}
-      {!menuAberto && (
-        <div className="botao-expandir" onClick={expandMenu}>
-          <FontAwesomeIcon icon={faAngleRight} />
-        </div>
-      )}
-      <ul className={menuAberto ? '' : 'oculto'} onClick={handleCloseMenu}>
-        <li>
-          <a href="#Dashboard">
-            <span className="icone icone-chart"><FontAwesomeIcon icon={faChartPie} /></span>
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#Dados">
-            <span className="icone icone-database"><FontAwesomeIcon icon={faDatabase} /></span>
-            Dados
-          </a>
-        </li>
-        <li>
-          <a href="#Rateio">
-            <span className="icone icone-brain"><FontAwesomeIcon icon={faBrain} /></span>
-            Rateio
-          </a>
-        </li>
-        <li>
-          <a href="#Configuração">
-            <span className="icone icone-cog"><FontAwesomeIcon icon={faCog} /></span>
-            Configuração
-          </a>
-        </li>
+        <ul className={`icones ${menuAberto ? '' : 'fechado'}`}>
+          <li>
+            <a href="#Dashboard">
+              <span className="icone icone-chart"><FontAwesomeIcon icon={faChartPie} /></span>
+              {menuAberto && <span>Dashboard</span>}
+            </a>
+          </li>
+          <li>
+            <a href="#Dados">
+              <span className="icone icone-database"><FontAwesomeIcon icon={faDatabase} /></span>
+              {menuAberto && <span>Dados</span>}
+            </a>
+          </li>
+          <li>
+            <a href="#Rateio">
+              <span className="icone icone-brain"><FontAwesomeIcon icon={faBrain} /></span>
+              {menuAberto && <span>Rateio</span>}
+            </a>
+          </li>
+          <li>
+            <a href="#Configuração">
+              <span className="icone icone-cog"><FontAwesomeIcon icon={faCog} /></span>
+              {menuAberto && <span>Configuração</span>}
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul className={`palavras ${menuAberto ? '' : 'oculto'}`} onClick={handleToggleMenu}>
+        
       </ul>
     </div>
   );
